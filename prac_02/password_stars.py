96dd94a811a12b2be0a340de56e6d7b91b8c05a9
AdriantoHartono <adrianto.hartono@my.jcu.edu.au>
2023-03-22 22:24:45 +0800

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -3,10 +3,16 @@
 
 MINIMUM = 10
 
-password = input("Enter Password: ")
-while len(password) < MINIMUM:
-    print("Your password doesn't meet the minimum length")
+def main():
+    password = get_password()
+    for i in range(0, len(password)):
+        print("*", end="")
+def get_password():
+    """Getting password as required"""
     password = input("Enter Password: ")
+    while len(password) < MINIMUM:
+        print("Your password doesn't meet the minimum length")
+        password = input("Enter Password: ")
+    return password
 
-for i in range(0, len(password)):
-    print("*", end="")
+main()
