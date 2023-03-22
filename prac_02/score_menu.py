085bd425eb52836d54500e64b8314845fe8a65a6
AdriantoHartono <adrianto.hartono@my.jcu.edu.au>
2023-03-22 22:23:41 +0800

    Added new file

---
+++ prac_02/score_menu.py
@@ -0,0 +1,58 @@
+# Adrianto Hartono
+# 14347930
+
+MENU ="""(G)et a valid score
+(P)rint result
+(S)how stars
+(Q)uit"""
+
+def main():
+    score = 0
+    print(MENU)
+    answer = input(">>> ").upper()
+    while answer != "Q":
+        if answer == "G":
+            score = get_valid_score("Enter score: ", 0, 100)
+            print(f"Your score : {score}")
+        elif answer == "P":
+            print(f"Your score: {score}")
+            result = determine_score(score)
+            print(f"Result    : {result}")
+        elif answer == "S":
+            print(f"Your score is {score}, and here is your star: ")
+            for i in range(score):
+                print("*", end="")
+            print()
+        else:
+            print("Invalid Option")
+        print()
+        print(MENU)
+        answer = input(">>> ").upper()
+    print("Farewell")
+
+def get_valid_score(prompt, low, high):
+    "Getting a valid score"
+    score = int(input(prompt))
+    while score < low or score > high:
+        print("Invalid score")
+        score = int(input(prompt))
+    return score
+
+def determine_score(score):
+    "Determining score criteria based on the score"
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+def get_score(prompt):
+    """Getting score"""
+    score = int(input(prompt))
+    return score
+
+
+main()
